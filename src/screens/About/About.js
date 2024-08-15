0708e7923a61ac8f6040c5c387295906b6dd43cc
Akash <akash00951@gmail.com>
2024-08-15 16:38:04 +0530

    added about us screen

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -1,45 +1,169 @@
-import React from 'react';
-import './About.css'; // Import the CSS file
-import Img from '../../assets/img/photo.jpg'
+import React from "react";
+import "./About.css"; // Import the CSS file
+import Power from "../../assets/img/power.png";
 const About = () => {
   return (
     <div className="about-us-container">
-      <div className="about-us-banner">
-        <h1>About Us</h1>
+      <div className="banner-container">
+        <div className="about-us-banner"></div>
       </div>
       <div className="about-us-content">
-        <p>
-          Welcome to ITradeTech, your trusted source for learning and mastering the art of trading. Our mission is to provide top-notch educational resources and training programs to help you succeed in the financial markets.
-        </p>
-        <p>
-          Our team of experienced professionals is dedicated to bringing you the latest insights, strategies, and tools needed to navigate the complex world of trading. Whether you're a beginner or an experienced trader, we have courses and resources tailored to meet your needs.
-        </p>
-        <p>
-          Join us on this journey to financial mastery and take your trading skills to the next level with ITradeTech.
-        </p>
-      </div>
-      <div className="team-section">
-        <h2>Meet Our Team</h2>
-        <div className="team-members">
-          <div className="team-member">
-            <img src={Img} alt="Team Member 1" className="team-member-image" />
-            <h3>John Doe</h3>
-            <p>Founder & CEO</p>
+        <div className="d-flex mb-3">
+          <div className="mr-4">
+            <img className="about-img" src={Power} alt="power" />
           </div>
-          <div className="team-member">
-            <img src={Img} alt="Team Member 2" className="team-member-image" />
-            <h3>Jane Smith</h3>
-            <p>Chief Technical Analyst</p>
+          <div className="mt-n1">
+            <h3 className="about-title fw-600">
+              Empowering Traders, One Trade at a Time
+            </h3>
+            <div className="devider"></div>
+            <ul className="center-align">
+              <li className="why-desc mt-1">
+                At I Trade Tech, we are more than just a trading academy – we
+                are a community dedicated to transforming the way you approach
+                the financial markets. Founded by experienced traders and
+                financial experts, our academy is built on the belief that with
+                the right knowledge and tools, anyone can achieve success in
+                trading.
+              </li>
+            </ul>
           </div>
-          <div className="team-member">
-            <img src={Img} alt="Team Member 3" className="team-member-image" />
-            <h3>Mike Johnson</h3>
-            <p>Senior Financial Advisor</p>
+        </div>
+
+        <div className="d-flex mb-3">
+          <div className="mt-n1">
+            <h3 className="about-title fw-600">Our Mission </h3>
+            <div className="devider"></div>
+            <ul className="center-align">
+              <li className="why-desc mt-1">
+                To provide accessible, high-quality trading education that
+                empowers individuals to take control of their financial futures.
+                We are committed to delivering a comprehensive learning
+                experience that equips you with the skills, strategies, and
+                confidence to trade effectively in any market environment.
+              </li>
+            </ul>
+          </div>
+          <div className="mr-4">
+            <img className="about-img" src={Power} alt="power" />
+          </div>
+        </div>
+
+        <div className="d-flex mb-3">
+          <div className="mr-4">
+            <img className="about-img" src={Power} alt="power" />
+          </div>
+          <div className="mt-n1">
+            <h3 className="about-title fw-600">Our Approach</h3>
+            <div className="devider"></div>
+            <ul className="center-align">
+              <li className="why-desc mt-1">
+                We offer a unique blend of theoretical knowledge and hands-on
+                experience. Our curriculum is designed to cater to traders of
+                all levels - from beginners just starting out, to seasoned
+                professionals looking to refine their strategies. Each course is
+                structured to provide a balanced mix of lectures, practical
+                exercises, and real-time trading simulations.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="d-flex mb-3">
+          <div className="mt-n1">
+            <h3 className="about-title fw-600">Our Values </h3>
+            <div className="devider"></div>
+            <ul className="center-align">
+              <li className="why-desc mt-1">
+                <b>Excellence in Education:</b> We strive to offer the most
+                relevant and up-to-date content, designed and delivered by
+                industry professionals. Our courses are meticulously crafted to
+                ensure you gain practical, real-world trading skills.
+              </li>
+              <li className="why-desc mt-1">
+                <b>Integrity &amp; Transparency:</b> We believe in teaching
+                trading in a way that is honest, transparent, and free from
+                unrealistic promises. Our focus is on sustainable growth and
+                long-term success, not quick fixes or get-rich-quick schemes.
+              </li>
+              <li className="why-desc mt-1">
+                <b>Community &amp; Support:</b> Trading can be a solitary
+                pursuit, but it doesn't have to be. At I Trade Tech, you'll find
+                a supportive community of like-minded individuals. We foster an
+                environment where students can share insights, ask questions,
+                and grow together.
+              </li>
+              <li className="why-desc mt-1">
+                <b>Continuous Improvement:</b> The financial markets are
+                constantly evolving, and so are we. We regularly update our
+                courses and resources to reflect the latest market trends and
+                trading techniques, ensuring you stay ahead of the curve.
+              </li>
+            </ul>
+          </div>
+          <div className="mr-4">
+            <img className="about-img" src={Power} alt="power" />
+          </div>
+        </div>
+
+        <div className="d-flex mb-3">
+          <div className="mr-4">
+            <img className="about-img" src={Power} alt="power" />
+          </div>
+          <div className="mt-n1">
+            <h3 className="about-title fw-600">Why Choose Us?</h3>
+            <div className="devider"></div>
+            <ul className="center-align">
+              <li className="why-desc mt-1">
+                <b>Expert Instructors:</b> Learn from seasoned traders with
+                22+years of experience in various financial markets. Our
+                instructors are passionate about teaching and are committed to
+                helping you succeed.
+              </li>
+              <li className="why-desc mt-1">
+                <b>Integrity &amp; Transparency:</b> We believe in teaching
+                trading in a way that is honest, transparent, and free from
+                unrealistic promises. Our focus is on sustainable growth and
+                long-term success, not quick fixes or get-rich-quick schemes.
+              </li>
+              <li className="why-desc mt-1">
+                <b>Community &amp; Support:</b> Trading can be a solitary
+                pursuit, but it doesn't have to be. At I Trade Tech, you'll find
+                a supportive community of like-minded individuals. We foster an
+                environment where students can share insights, ask questions,
+                and grow together.
+              </li>
+              <li className="why-desc mt-1">
+                <b>Continuous Improvement:</b> The financial markets are
+                constantly evolving, and so are we. We regularly update our
+                courses and resources to reflect the latest market trends and
+                trading techniques, ensuring you stay ahead of the curve.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="d-flex mb-3">
+          <div className="mt-n1">
+            <h3 className="about-title fw-600">Join Us</h3>
+            <div className="devider"></div>
+            <ul className="center-align">
+              <li className="why-desc mt-1">
+                Are you ready to take the first step towards mastering the
+                markets? Join I TRADE TECH today and start your journey to
+                becoming a confident, successful trader. Together, we’ll
+                navigate the complexities of the financial markets and unlock
+                your full trading potential.
+              </li>
+            </ul>
+          </div>
+          <div className="mr-4">
+            <img className="about-img" src={Power} alt="power" />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default About;
